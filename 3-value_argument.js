dc5609a6e5209c4bc29ed81aa37d216ddb65b34d
Kmotasi <129315334+Kmotasi@users.noreply.github.com>
2025-05-24 17:37:27 +0200

    Add files via upload

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,12 +3,10 @@ const args = process.argv.slice(2);
 if (args[0] === undefined) 
 {
   console.log("No argument");
-}
-else if(args[1] === undifined)
+} else if (args[1] === undefined) 
 {
-	console.log("HBTN")
-}
-else 
+  console.log("one argument " + args[0]);
+} else 
 {
-	console.log("HBTN cool")
+  console.log("Multiple arguments");
 }
